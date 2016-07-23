86aa01caf0d5f782eaaceefaf3bd7fc685223d8f
Nick Matantsev <nick.matantsev@gmail.com>
2016-07-23 01:35:51 -0400

    Promisify camera operations

--- index.js
+++ index.js
@@ -1,41 +1,68 @@
 var fs = require('fs');
 var gphoto2 = require('gphoto2');
+var Promise = require('bluebird');
 
 var GPhoto = new gphoto2.GPhoto2();
 
-// list cameras
-GPhoto.list(function (list) {
-    if (list.length !== 1) throw new Error('single camera expected');
+function findCamera() {
+    return new Promise(function (resolve, reject) {
+        GPhoto.list(function (list) {
+            if (list.length !== 1) {
+                reject(new Error('single camera expected'));
+                return;
+            }
 
-    var camera = list[0];
-    console.log('using camera:', camera.model);
+            var camera = list[0];
+            console.log('using camera:', camera.model);
 
-    // // get configuration tree
-    // camera.getConfig(function (er, settings) {
-    //     console.log(settings);
-    // });
+            resolve(camera);
+        });
+    });
+}
 
-    // Set configuration values
-    camera.setConfigValue('capture', 'on', function (er) {
-        if (er) {
-            throw new Error('got error: ' + er);
-        }
+function setCameraConfig(camera, name, value) {
+    return new Promise(function (resolve, reject) {
+        camera.setConfigValue(name, value, function (er) {
+            if (er) {
+                reject(new Error('error setting config: ' + er));
+                return;
+            }
 
+            resolve(camera);
+        });
+    });
+}
+
+function takePicture(camera) {
+    return new Promise(function (resolve, reject) {
         camera.takePicture({
             download: true
         }, function (er, output) {
             if (er) {
-                throw new Error('got error: ' + er);
+                reject(new Error('got error: ' + er));
+                return;
             }
 
-            console.log('writing output size:', output.length);
-            fs.writeFile(__dirname + '/picture.cr2', output, function (err) {
-                if (err) {
-                    throw new Error('got error writing file: ' + err);
-                }
-
-                console.log('wrote output file!');
-            });
+            resolve(output);
         });
+    });
+}
+
+findCamera().then(function (camera) {
+    return setCameraConfig(camera, 'capture', 'on');
+}).then(function (camera) {
+    return takePicture(camera).then(function (output) {
+        console.log('writing output size:', output.length);
+
+        // async file write on the side
+        fs.writeFile(__dirname + '/picture.jpg', output, function (err) {
+            if (err) {
+                throw new Error('got error writing file: ' + err);
+            }
+
+            console.log('wrote output file!');
+        });
+
+        return camera;
     });
 });
