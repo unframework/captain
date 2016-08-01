79a41545ed2c985b89f144359ebc0529dc7a760f
Nick Matantsev <nick.matantsev@gmail.com>
2016-08-01 15:04:59 -0400

    Move gphoto2-specific code into dedicated module

--- index.js
+++ index.js
@@ -1,86 +1,8 @@
 var fs = require('fs');
 var Promise = require('bluebird');
 
-var GPhoto = new gphoto2.GPhoto2();
-
 var frameCount = parseInt(process.argv[2], 10) || (function () { throw new Error('need frame count'); })();
 var delayMillis = parseInt(process.argv[3], 10) || (function () { throw new Error('need delay in millis'); })();
-
-function findCamera() {
-    return new Promise(function (resolve, reject) {
-        var GPhoto = new require('gphoto2').GPhoto2();
-
-        GPhoto.list(function (list) {
-            if (list.length !== 1) {
-                reject(new Error('single camera expected'));
-                return;
-            }
-
-            var camera = list[0];
-            console.log('using camera:', camera.model);
-
-            resolve(camera);
-        });
-    });
-}
-
-function walkSettingsMap(obj, cb) {
-    var keys = Object.keys(obj);
-    keys.forEach(function (key) {
-        var info = obj[key];
-
-        cb(key, info);
-
-        if (info.children) {
-            walkSettingsMap(info.children, cb);
-        }
-    });
-}
-
-function getCameraConfig(camera, name) {
-    return new Promise(function (resolve, reject) {
-        camera.getConfig(function (er, settings) {
-            var value = null;
-
-            walkSettingsMap(settings, function (key, info) {
-                if (key === name) {
-                    value = info.value;
-                    console.log(key + ':', info.label, '(' + info.type + (info.choices ? '; ' + info.choices.join(', ') : '') + ') =', info.value);
-                }
-            });
-
-            resolve(value);
-        });
-    });
-}
-
-function setCameraConfig(camera, name, value) {
-    return new Promise(function (resolve, reject) {
-        camera.setConfigValue(name, value, function (er) {
-            if (er) {
-                reject(new Error('error setting config: ' + er));
-                return;
-            }
-
-            resolve(camera);
-        });
-    });
-}
-
-function storePicture(camera) {
-    return new Promise(function (resolve, reject) {
-        camera.takePicture({
-            download: false,
-        }, function (er) {
-            if (er) {
-                reject(new Error('got error: ' + er));
-                return;
-            }
-
-            resolve();
-        });
-    });
-}
 
 function runSeries(frameCount, delayMillis, doFrame) {
     var counter = 0;
@@ -113,9 +35,9 @@ function runSeries(frameCount, delayMillis, doFrame) {
     });
 }
 
-findCamera().then(function (camera) {
+require('./lib/findCamera').then(function (camera) {
     // save files locally
-    return setCameraConfig(camera, 'capturetarget', 'Memory card'); // per http://gphoto-software.10949.n7.nabble.com/Problem-setting-capturetarget-on-Canon-G9-td13758.html
+    return camera.setConfig('capturetarget', 'Memory card').then(function () { return camera; }); // per http://gphoto-software.10949.n7.nabble.com/Problem-setting-capturetarget-on-Canon-G9-td13758.html
 }).then(function (camera) {
     var prefix = 'pic_' + new Date().getTime() + '_';
 
@@ -124,7 +46,7 @@ findCamera().then(function (camera) {
 
         var fileName = prefix + counter + '.jpg';
 
-        return storePicture(camera).then(function () {
+        return camera.storePicture().then(function () {
             console.log(new Date(), 'done frame:', counter);
         });
     });
